eb9546e7695ace0e920e85fdb0d1228b07145e2b
Sergey Makukha <sergey.makukha@gmail.com>
2020-04-20 14:26:46 +0300

    Changes after review

--- jni/src/main/java/com/horizen/sigproofnative/NaiveThresholdSigProof.java
+++ jni/src/main/java/com/horizen/sigproofnative/NaiveThresholdSigProof.java
@@ -1,5 +1,6 @@
 package com.horizen.sigproofnative;
 
+import com.horizen.librustsidechains.FieldElement;
 import com.horizen.schnorrnative.SchnorrPublicKey;
 import com.horizen.schnorrnative.SchnorrSignature;
 
@@ -7,17 +8,25 @@ import java.util.List;
 
 public class NaiveThresholdSigProof {
 
+    private static native FieldElement nativeCreateMsgToSign(BackwardTransfer[] bt,
+                                                             byte[] endEpochBlockHash, byte[] prevEndEpochBlockHash);
+
+    public static FieldElement createMsgToSign(BackwardTransfer[] bt,
+                                               byte[] endEpochBlockHash, byte[] prevEndEpochBlockHash) {
+        return nativeCreateMsgToSign(bt, endEpochBlockHash, prevEndEpochBlockHash);
+    }
+
     private static native byte[] nativeCreateProof(BackwardTransfer[] bt,
                                                    byte[] endEpochBlockHash, byte[] prevEndEpochBlockHash,
                                                    SchnorrSignature[] schnorrSignatures, SchnorrPublicKey[] schnorrPublicKeys,
-                                                   long threshold, String verificationKeyPath);
+                                                   long threshold, String provingKeyPath);
 
     public static byte[] createProof(List<BackwardTransfer> btList,
                                      byte[] endEpochBlockHash, byte[] prevEndEpochBlockHash,
                                      List<SchnorrSignature> schnorrSignatureList, List<SchnorrPublicKey> schnorrPublicKeyList,
-                                     long threshold, String verificationKeyPath) {
+                                     long threshold, String provingKeyPath) {
         return nativeCreateProof(btList.toArray(new BackwardTransfer[0]), endEpochBlockHash, prevEndEpochBlockHash,
                 schnorrSignatureList.toArray(new SchnorrSignature[0]), schnorrPublicKeyList.toArray(new SchnorrPublicKey[0]),
-                threshold, verificationKeyPath);
+                threshold, provingKeyPath);
     }
 }
---
+++ jni/src/test/java/com/horizen/sigproofnative/NaiveThresholdSigProofTest.java
@@ -0,0 +1,78 @@
+package com.horizen.sigproofnative;
+
+import com.horizen.librustsidechains.FieldElement;
+import com.horizen.schnorrnative.SchnorrKeyPair;
+import com.horizen.schnorrnative.SchnorrPublicKey;
+import com.horizen.schnorrnative.SchnorrSignature;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class NaiveThresholdSigProofTest {
+
+    static int keyCount = 3;
+    static int threshold = 2;
+
+    byte[] endEpochBlockHash = new byte[32];
+    byte[] prevEndEpochBlockHash = new byte[32];
+
+    static int backwardTransferCout = 5;
+
+    List<SchnorrKeyPair> keyPairList = new ArrayList<>();
+    List<SchnorrPublicKey> publicKeyList = new ArrayList<>();
+    List<SchnorrSignature> signatureList = new ArrayList<>();
+
+    List<BackwardTransfer> btList = new ArrayList<>();
+
+    @Before
+    public void testGenerate() {
+
+        for(int i = 0; i < backwardTransferCout; i++) {
+
+            byte[] publicKeyHash = new byte[32];
+            long amount = i;
+
+            btList.add(new BackwardTransfer(publicKeyHash, amount));
+        }
+
+
+        for (int i = 0; i<keyCount; i++) {
+            SchnorrKeyPair keyPair = SchnorrKeyPair.generate();
+
+            assertNotNull("Key pair generation was unsuccessful.", keyPair);
+            assertTrue("Public key verification failed.", keyPair.getPublicKey().verifyKey());
+
+            keyPairList.add(keyPair);
+            publicKeyList.add(keyPair.getPublicKey());
+        }
+
+        for (int i = 0; i<keyCount; i++) {
+            if (i < threshold) {
+                FieldElement msgToSign = NaiveThresholdSigProof.createMsgToSign(btList.toArray(new BackwardTransfer[0]),
+                        endEpochBlockHash, prevEndEpochBlockHash);
+                signatureList.add(keyPairList.get(i).signMessage(msgToSign));
+            } else {
+                signatureList.add(new SchnorrSignature());
+            }
+        }
+
+    }
+
+    @Test
+    public void testCreateProof() {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        String verificationKeyPath = new File(classLoader.getResource("sample_params").getFile()).getAbsolutePath();
+        byte[] proof = NaiveThresholdSigProof.createProof(btList, endEpochBlockHash, prevEndEpochBlockHash,
+                signatureList, publicKeyList, threshold, verificationKeyPath);
+
+        assertNotNull("Proof must be not null.", proof);
+    }
+}
