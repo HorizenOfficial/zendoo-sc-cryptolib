74b24f752e34f40f733da21d5112cbb48658b0b8
VolodymyrBabaiev <Volodymyr.Babaiev@infopulse.com>
2021-03-25 19:38:38 +0200

    Fixed existence and absence build errors.

--- jni/src/main/java/com/horizen/commitmenttree/ScExistenceProof.java
+++ jni/src/main/java/com/horizen/commitmenttree/ScExistenceProof.java
@@ -1,26 +1,23 @@
 package com.horizen.commitmenttree;
 
-public class ScExistanceProof implements AutoCloseable {
+import com.horizen.librustsidechains.Library;
+
+public class ScExistenceProof implements AutoCloseable {
     private long existanceProofPointer;
 
     static {
         Library.load();
     }
 
-    private ScExistanceProof(long existanceProofPointer) {
+    private ScExistenceProof(long existanceProofPointer) {
         if (existanceProofPointer == 0)
             throw new IllegalArgumentException("existanceProofPointer must be not null.");
         this.existanceProofPointer = existanceProofPointer;
     }
 
-    @Override
-    public void close() throws Exception {
-        freeCommitmentTree();
-    }
-
     private static native void nativeFreeScExistanceProof(long existanceProofPointer);
 
-    // Free Rust memory from CommitmentTree
+    // Free Rust memory
     public void freeScExistanceProof() {
         if (existanceProofPointer != 0) {
             nativeFreeScExistanceProof(this.existanceProofPointer);
