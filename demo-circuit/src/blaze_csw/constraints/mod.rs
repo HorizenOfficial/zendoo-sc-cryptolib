d93cad1b7ef11165519bda9cedcef7981bf77fe1
Paolo Tagliaferri <paolotagliaferri@horizenlabs.io>
2021-12-17 14:22:08 +0100

    Implemented UT for FT with wrong amount

--- demo-circuit/src/blaze_csw/constraints/mod.rs
+++ demo-circuit/src/blaze_csw/constraints/mod.rs
@@ -1333,6 +1333,38 @@ mod test {
     }
 
     #[test]
+    fn test_csw_circuit_ft_wrong_amount() {
+        let (sidechain_id, num_custom_fields, num_commitment_hashes, constant, debug_only) =
+            generate_circuit_test_data();
+
+        let mut csw_prover_data = generate_test_csw_prover_data(
+            CswType::FT,
+            sidechain_id,
+            num_custom_fields,
+            num_commitment_hashes,
+            None,
+            None,
+        );
+
+        // Change the value of the amount
+        csw_prover_data.sys_data.amount += 1;
+
+        let failing_constraint = test_csw_circuit(
+            debug_only,
+            sidechain_id,
+            num_custom_fields,
+            num_commitment_hashes,
+            constant,
+            csw_prover_data,
+            None,
+        );
+        println!("Failing constraint: {:?}", failing_constraint);
+        assert!(failing_constraint
+            .unwrap()
+            .contains("input.amount == sys_data.amount"));
+    }
+
+    #[test]
     fn test_csw_circuit_with_custom_keys() {
         let sidechain_id = FieldElement::from(77u8);
         let num_custom_fields = 1;
