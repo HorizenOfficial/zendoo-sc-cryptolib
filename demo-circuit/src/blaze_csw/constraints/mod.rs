26b97e7b6f29428e6e557c0b560472f7d03df40e
Paolo Tagliaferri <paolotagliaferri@horizenlabs.io>
2021-12-17 10:56:49 +0100

    Implemented UT for UTXO with wrong nullifier

--- demo-circuit/src/blaze_csw/constraints/mod.rs
+++ demo-circuit/src/blaze_csw/constraints/mod.rs
@@ -1071,6 +1071,37 @@ mod test {
     }
 
     #[test]
+    fn test_csw_circuit_utxo_wrong_nullifier() {
+        let (sidechain_id, num_custom_fields, num_commitment_hashes, constant, debug_only) =
+            generate_circuit_test_data();
+
+        let mut csw_prover_data = generate_test_csw_prover_data(
+            CswType::UTXO,
+            sidechain_id,
+            num_custom_fields,
+            num_commitment_hashes,
+            None,
+            None,
+        );
+
+        // Change the value of the nullifier
+        csw_prover_data.sys_data.nullifier.double_in_place();
+
+        let failing_constraint = test_csw_circuit(
+            debug_only,
+            sidechain_id,
+            num_custom_fields,
+            num_commitment_hashes,
+            constant,
+            csw_prover_data,
+        );
+        println!("Failing constraint: {:?}", failing_constraint);
+        assert!(failing_constraint
+            .unwrap()
+            .contains("require(nullifier == outputHash)"));
+    }
+
+    #[test]
     fn test_csw_circuit_with_custom_keys() {
         let sidechain_id = FieldElement::from(77u8);
         let num_custom_fields = 1;
