c5ee523202b795d5a2048ec44f6f4986619fe989
Yuri Sibirski <yuri@horizenlabs.io>
2022-08-04 13:58:04 -0400

    Testing updated CI/CD publish

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -2,12 +2,12 @@
 
 set -eo pipefail
 
+export CONTAINER_PUBLISH="false"
+PUBLISH_BUILD="${PUBLISH_BUILD:-false}"
 pom_version="$(xpath -q -e '/project/version/text()' jni/pom.xml)"
 
 echo "TRAVIS_TAG: $TRAVIS_TAG"
 echo "jni/pom.xml version: $pom_version"
-
-export CONTAINER_PUBLISH="false"
 
 # Functions
 function import_gpg_keys() {
@@ -37,28 +37,65 @@ function check_signed_tag() {
 # empty key.asc file in case we're not signing
 touch "${HOME}/key.asc"
 
-if [ -n "${TRAVIS_TAG}" ]; then
+if [ -n "${TRAVIS_TAG}" ] && [ "${PUBLISH_BUILD}" = true ]; then
   # checking if MAINTAINER_KEYS is set
   if [ -z "${MAINTAINER_KEYS}" ]; then
     echo "MAINTAINER_KEYS variable is not set. Make sure to set it up for release build!!!"
+    exit 1
   fi
 
   # shellcheck disable=SC2155
   export GNUPGHOME="$(mktemp -d 2>/dev/null || mktemp -d -t 'GNUPGHOME')"
+
+  # Checking PROD vs DEV build
+  echo "The current production branch is: ${PROD_RELEASE_BRANCH}"
+
   import_gpg_keys "${MAINTAINER_KEYS}"
 
   if (check_signed_tag "${TRAVIS_TAG}"); then
     if [[ "${IMAGE_TAG}" != *"nightly"* ]]; then
-      if ! [[ ${TRAVIS_TAG} =~ "${pom_version}"[0-9]*$ ]]; then
-        echo -e "\nTag format differs from the pom file version or pom file version is in the wrong format"
-        echo -e "Github tag name: ${TRAVIS_TAG}\nPom file version: ${pom_version}.\nPublish stage is NOT going to run!!!"
-      else
-        echo "" && echo "=== Release build ===" && echo ""
-        export CONTAINER_PUBLISH="true"
+      # Prod vs dev release
+      if ( git branch -r --contains "${TRAVIS_TAG}" | grep -xqE ". origin\/${PROD_RELEASE_BRANCH}$" ); then
+        # Checking if package version matches PROD release version
+        if ! [[ "${pom_version}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+          echo "Aborting, pom version is in the wrong format."
+          exit 1
+        fi
+
+        # Checking Github tag format
+        if ! [[ "${TRAVIS_TAG}" == "${pom_version}" ]]; then
+          echo -e "\nTag format differs from the pom file version."
+          echo -e "Github tag name: ${TRAVIS_TAG}\nPom file version: ${pom_version}.\nPublish stage is NOT going to run!!!"
+          exit 1
+        fi
+
+        # Announcing PROD release
+        echo "" && echo "=== Production release build ===" && echo ""
         echo "Fetching maven gpg signing keys."
         curl -sLH "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3.raw" "${MAVEN_KEY_ARCHIVE_URL}" |
           openssl enc -d -aes-256-cbc -md sha256 -pass pass:"${MAVEN_KEY_ARCHIVE_PASSWORD}" |
           tar -xzf- -C "${HOME}"
+        export CONTAINER_PUBLISH="true"
+      else
+        # Checking if package version matches DEV release version
+        if ! [[ "${pom_version}" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT){1}$ ]]; then
+          echo "Aborting, package version is in the wrong format."
+          exit 1
+        fi
+
+        # Checking Github tag format
+        if ! [[ "${TRAVIS_TAG}" =~ "${pom_version}"[0-9]*$ ]]; then
+          echo "Aborting, tag format differs from the version under build.sbt file."
+          exit 1
+        fi
+
+        # Announcing DEV release
+        echo "" && echo "=== Development release build ===" && echo ""
+        echo "Fetching maven gpg signing keys."
+        curl -sLH "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3.raw" "${MAVEN_KEY_ARCHIVE_URL}" |
+          openssl enc -d -aes-256-cbc -md sha256 -pass pass:"${MAVEN_KEY_ARCHIVE_PASSWORD}" |
+          tar -xzf- -C "${HOME}"
+        export CONTAINER_PUBLISH="true"
       fi
     fi
   fi
